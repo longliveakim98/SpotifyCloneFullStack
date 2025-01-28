7584b99cfe8e0dc5baadcc7e94f13af50c7cc5c1
longliveakim98 <mohdaimanhakim.98@gmail.com>
2025-01-28 17:26:58 +0800

    cloudinary redo

--- spotify-clone-server/server.js
+++ spotify-clone-server/server.js
@@ -22,7 +22,7 @@ app.use(cors()); // allow frontend to make requests to backend, if frontend runs
 //initializing routes
 app.use("/api/song", songRouter);
 app.use("/api/album", albumRouter);
-app.use("/api/cloudinary", cloudinaryRouter);
+app.use("/api/cloudinary-signature", cloudinaryRouter);
 app.get("/", (req, res) => res.send("API WORKING"));
 
 app.listen(port, () => console.log(`Server started on ${port}`));
--- spotify-clone-server/src/routes/cloudinaryRouter.js
+++ spotify-clone-server/src/routes/cloudinaryRouter.js
@@ -3,6 +3,6 @@ import { getCloudinarySignature } from "../config/cloudinary.js";
 
 const cloudinaryRouter = express.Router();
 
-cloudinaryRouter.get("/api/cloudinary-signature", getCloudinarySignature);
+cloudinaryRouter.get("/", getCloudinarySignature);
 
 export default cloudinaryRouter;
