f45461f9a714619a398867074c9707e4db1613ea
longliveakim98 <mohdaimanhakim.98@gmail.com>
2025-01-29 15:10:08 +0800

    addsong

--- spotify-admin/src/pages/AddSong.jsx
+++ spotify-admin/src/pages/AddSong.jsx
@@ -15,7 +15,7 @@ const AddSong = () => {
 
   const getCloudinarySignature = async (name) => {
     const res = await axios.get(`/api/cloudinary-signature?name=${name}`);
-    const data = await res.json();
+    const data = await res.data;
     return data;
   };
 
